59510aa9c414cb772482f2e6eb5d61e08ed0768a
sarasate <luis@superlumen.io>
2018-09-07 23:15:19 +0200

    chore: remove `polyfill` from babel presets

--- .babel-preset.js
+++ .babel-preset.js
@@ -47,7 +47,7 @@ function preset(context, options = {}) {
         {
           // we are only polyfilling the node environment
           // so we need to enable the runtime replacements for the browser preset
-          polyfill: !!browser,
+          // polyfill: !!browser,
         },
       ],
     ],
